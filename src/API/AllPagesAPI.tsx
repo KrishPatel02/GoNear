b8e3639e691f7bbfdc4f0011dbc3038f92ddc085
kihan2518B <kp648027@gmail.com>
2024-07-31 00:17:37 +0530

    Navbar api for different user and some ui changes

--- src/API/AllPagesAPI.tsx
+++ src/API/AllPagesAPI.tsx
@@ -1,13 +1,119 @@
-const NavBarHemburgMenuAPI = [
+import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import HeadsetMicRoundedIcon from "@mui/icons-material/HeadsetMicRounded";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+
+import { FaUser } from "react-icons/fa";
+import { FaUserEdit } from "react-icons/fa";
+
+import { FiPackage } from "react-icons/fi";
+import { TbListDetails, TbShoppingBagPlus } from "react-icons/tb";
+import AnalyticsRoundedIcon from "@mui/icons-material/AnalyticsRounded";
+
+const iconClassName = "h-5 w-5 cursor-pointer";
+
+export const NavBarHemburgMenuCustomerAPI = [
     {
         title: "Profile Details",
-        href: "",
-        icon: "",
-        customerHemburg: "",    // True or False
-        sellerHemburg: "",     // True or False  
-        customerDashboard: "", // True or False
-        sellerDashboard: "",   // True or False
+
+        href: "CustomerDashboard/MyProfile",
+
+        icon: <FaUser className={iconClassName} />,
+    },
+
+    {
+        title: "My Orders",
+
+        href: "/CustomerDashboard/MyProfile/MyOrders",
+
+        icon: <FiPackage className={iconClassName} />,
+    },
+
+    {
+        title: "My Cart",
+
+        href: "/CustomerDashboard/MyProfile/MyCart",
+
+        icon: <ShoppingCartRoundedIcon className={iconClassName} />,
+    },
+
+    {
+        title: "Wishlist",
+
+        href: "/CustomerDashboard/MyProfile/Wishlist",
+
+        icon: <FavoriteBorderRoundedIcon className={iconClassName} />,
+    },
+
+    {
+        title: "Edit Profile",
+
+        href: "CustomerDashboard/MyProfile/EditProfile",
+
+        icon: <FaUserEdit className={iconClassName} />,
+    },
+
+    {
+        title: "Customer Care",
+
+        href: "/CustomerDashboard/CustomerCare",
+
+        icon: <HeadsetMicRoundedIcon className={iconClassName} />,
+    },
+
+    {
+        title: "Setting",
+
+        href: "CustomerDashboard/Setting",
+
+        icon: <SettingsOutlinedIcon className={iconClassName} />,
     },
 ];
 
-export default NavBarHemburgMenuAPI;
+export const NavBarHemburgMenuSellerAPI = [
+    {
+        title: "Profile Details",
+
+        href: "SellerDashboard/MyProfile",
+
+        icon: <FaUser className={iconClassName} />,
+    },
+    {
+        title: "Edit Profile",
+
+        href: "SellerDashboard/MyProfile/EditProfile",
+
+        icon: <FaUserEdit className={iconClassName} />,
+    },
+
+    {
+        title: "My Products",
+
+        href: "/SellerDashboard/Products",
+
+        icon: <TbListDetails className={iconClassName} />,
+    },
+    {
+        title: "Add Product",
+
+        href: "/SellerDashboard/Products/AddProduct",
+
+        icon: <TbShoppingBagPlus className={iconClassName} />,
+    },
+
+    {
+        title: "Analytics",
+
+        href: "/SellerDashboard/Analytics",
+
+        icon: <AnalyticsRoundedIcon className={iconClassName} />,
+    },
+
+    {
+        title: "Setting",
+
+        href: "SellerDashboard/Setting",
+
+        icon: <SettingsOutlinedIcon className={iconClassName} />,
+    },
+];
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,24 +3,26 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import PrimaryButton from "../UI/PrimaryButton";
 import { usePathname } from "next/navigation";
-import AllPagesAPI from "../API/AllPagesAPI"
+import { useUserData } from "@/Context/UserDataContext";
+import { Customer, Seller } from "@/types";
+import { NavBarHemburgMenuCustomerAPI, NavBarHemburgMenuSellerAPI } from "@/API/AllPagesAPI";
 
 const NavBar = () => {
     const pathname = usePathname();
 
-    const [loggedInUser, setLoggedInUser] = useState(null);
+    const [loggedInUser, setLoggedInUser] = useState<Customer | Seller>(null);
 
     const [menuOpen, setMenuOpen] = useState(false);
 
-    // const { state } = useUserData();
-    // const { user } = state;
+    const { state } = useUserData();
+    // console.log(state)
     useEffect(() => {
-        const UserData = localStorage.getItem("User") ?? null;
-
-        if (UserData != null) {
-            setLoggedInUser(JSON.parse(UserData));
+        if (state.customer) {
+            setLoggedInUser(state.customer);
+        } else if (state.seller) {
+            setLoggedInUser(state.seller);
         }
-    }, []);
+    }, [state]);
 
     const handleMenuToggle = () => {
         setMenuOpen(!menuOpen);
@@ -36,6 +38,8 @@ const NavBar = () => {
         setFalse();
     };
 
+    console.log("loggedInUser", loggedInUser)
+
     const navBarLinksClassName =
         "flex items-center px-4 py-2 gap-2 transition-colors duration-200 ";
 
@@ -44,18 +48,18 @@ const NavBar = () => {
     const unActivePathClassNameHemBurg =
         " hover:text-colorOne hover:font-medium";
     const activePathClassNameNavBar = "text-colorOne font-medium";
-    // if (state.loading) {
-    //     return <>Loading...</>;
-    // }
-    // if (state.error) {
-    //     console.log(state.error);
-    // }
+    if (state.loading) {
+        return <>Loading...</>;
+    }
+    if (state.error) {
+        console.log(state.error);
+    }
     return (
-        <div className="p-3 w-screen bg-white flex justify-evenly items-center fixed ">
+        <nav className="p-3 z-50 w-screen bg-white flex justify-between items-center fixed ">
             <Link
                 onClick={setFalse}
                 href="/"
-                className="text-2xl font-bold w-1/10 text-colorOne"
+                className="text-2xl font-bold w-full text-colorOne"
             >
                 <span className="text-black">
                     Go
@@ -63,38 +67,28 @@ const NavBar = () => {
                 Near
             </Link>
 
-            <div className="categories flex items-center gap-7 w-2/3">
-                <div className="relative w-1/2 flex items-evenly rounded-lg bg-colorFour focus:border-gray-500 border-gray-300">
-                    <button className=" w-1/12 place-content-center rounded-lg bg-colorFour focus:outline-none">
+            <div className="categories flex items-center justify-center gap-7 w-full">
 
-                    </button>
-
-                    <input
-                        type="text"
-                        placeholder="Search for products"
+                {!state.seller &&
+                    <Link
+                        href="/BecomeSeller"
                         onClick={setFalse}
-                        className="w-full py-2 px-4 bg-colorFour focus:outline-none rounded-lg "
-                    />
-                </div>
+                        className={`${navBarLinksClassName} ${pathname === "/BecomeSeller"
+                            ? `${activePathClassNameNavBar}`
+                            : ` ${unActivePathClassNameHemBurg}`
+                            }`}
+                    >
 
-                <Link
-                    href="/BecomeSeller"
-                    onClick={setFalse}
-                    className={`${navBarLinksClassName} ${pathname === "/BecomeSeller"
-                        ? `${activePathClassNameNavBar}`
-                        : ` ${unActivePathClassNameHemBurg}`
-                        }`}
-                >
-
-                    Become a Seller
-                </Link>
+                        Become a Seller
+                    </Link>
+                }
             </div>
 
-            <div className="flex items-center w-1/10">
+            <div className="flex items-center w-full justify-end">
                 {loggedInUser && (
                     <div className="relative flex  items-center ">
                         <PrimaryButton
-                            value={` Hy, `}
+                            value={` Hy, ${loggedInUser?.FullName.split(" ")[0]}`}
                             logo={``}
                             className={` flex align-middle p-2 text-white  ${menuOpen ? "bg-colorOne " : "bg-colorOne "
                                 }`}
@@ -103,23 +97,48 @@ const NavBar = () => {
 
                         {menuOpen && (
                             <div className="absolute top-10 right-0 mt-2 w-60 p-2 bg-transparent rounded-lg shadow-xl z-10">
-                                {AllPagesAPI.map((item) => {
-                                    return (
-                                        <Link
-                                            onClick={handleMenuToggle}
-                                            href={item.href}
-                                            className={` ${navBarLinksClassName}   ${pathname === item.href
-                                                ? `${activePathClassNameHemBurg}`
-                                                : ` ${unActivePathClassNameHemBurg}`
-                                                }`}
-                                            key={item.href}
-                                        >
-                                            {item.icon}
+                                {/* If user is Customer then Show Customer routes */}
+                                {state.customer &&
+                                    (NavBarHemburgMenuCustomerAPI.map((item) => {
+                                        return (
+                                            <Link
+                                                onClick={handleMenuToggle}
+                                                href={item.href}
+                                                className={` ${navBarLinksClassName}   ${pathname === item.href
+                                                    ? `${activePathClassNameHemBurg}`
+                                                    : ` ${unActivePathClassNameHemBurg}`
+                                                    }`}
+                                                key={item.href}
+                                            >
+                                                {item.icon}
 
-                                            {item.title}
-                                        </Link>
-                                    );
-                                })}
+                                                {item.title}
+                                            </Link>
+                                        );
+                                    })
+                                    )
+                                }
+                                {/* If user is Seller then Show Seller Routes */}
+                                {state.seller &&
+                                    (NavBarHemburgMenuSellerAPI.map((item) => {
+                                        return (
+                                            <Link
+                                                onClick={handleMenuToggle}
+                                                href={item.href}
+                                                className={` ${navBarLinksClassName}   ${pathname === item.href
+                                                    ? `${activePathClassNameHemBurg}`
+                                                    : ` ${unActivePathClassNameHemBurg}`
+                                                    }`}
+                                                key={item.href}
+                                            >
+                                                {item.icon}
+
+                                                {item.title}
+                                            </Link>
+                                        );
+                                    })
+                                    )
+                                }
 
                                 <Link
                                     href="/Logout"
@@ -144,7 +163,7 @@ const NavBar = () => {
                     </Link>
                 )}
             </div>
-        </div>
+        </nav>
     );
 };
 
