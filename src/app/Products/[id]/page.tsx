b813f7919c33d5d43f76fe370f87eab610103ac8
pu9052473 <pu9052473@gmail.com>
2024-08-05 17:21:20 +0530

    solve the bugs

--- src/app/Products/[id]/page.tsx
+++ src/app/Products/[id]/page.tsx
@@ -7,12 +7,17 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Product } from '@/types';
 import { db } from '@/Firebase/FirebaseConfig';
-import Link from 'next/link';
+import PrimaryButton from '@/UI/PrimaryButton';
+import Image from 'next/image';
+import { useUserData } from '@/Context/UserDataContext';
 
 const ProductDetails: React.FC = () => {
   const { id } = useParams();
   const productId = id as string;
   // console.log("productId", productId)
+
+  const { state } = useUserData();
+  // console.log("state",state);
 
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
@@ -46,24 +51,32 @@ const ProductDetails: React.FC = () => {
   }, [productId]);
 
   const handleAddToCart = () => {
-    if (!product) return;
+    if (state.customer){
+      if (!product) return;
 
-    // Retrieve the existing cart from localStorage
-    const existingCart = localStorage.getItem('MyCart');
-    const cart = existingCart ? JSON.parse(existingCart) : {};
+      // Retrieve the existing cart from localStorage
+      const existingCart = localStorage.getItem('MyCart');
+      const cart = existingCart ? JSON.parse(existingCart) : {};
 
-    // Check if the product is already in the cart
-    if (cart[productId]) {
-      // Increase the quantity of the existing product
-      cart[productId].quantity += 1;
-    } else {
-      // Add the new product with quantity 1
-      cart[productId] = { ...product, quantity: 1 };
+      // Check if the product is already in the cart
+      if (cart[productId]) {
+        // Increase the quantity of the existing product
+        cart[productId].quantity += 1;
+      } else {
+        // Add the new product with quantity 1
+        cart[productId] = { ...product, quantity: 1 };
+      }
+
+      // Update the cart in localStorage
+      localStorage.setItem('MyCart', JSON.stringify(cart));
+      toast.success('Product added to cart');
     }
-
-    // Update the cart in localStorage
-    localStorage.setItem('MyCart', JSON.stringify(cart));
-    toast.success('Product added to cart');
+    else if (state.seller){
+      toast.error('Only Customer Can Add Product To My Cart');
+    }
+    else {
+      toast.error('You Have To Login For Add Product To My Cart');
+    }
   };
 
   const handleRemoveFromCart = () => {
@@ -82,24 +95,20 @@ const ProductDetails: React.FC = () => {
   }
 
   return (
-    <div className='py-5 mx-auto w-fit'>
-      <h1 className='text-2xl font-bold mb-4'>{product.productName}</h1>
+    <div className='py-5 mt-16 flex flex-col gap-2 mx-auto w-fit'>
+      <h1 className='text-2xl font-bold'>{product.productName}</h1>
       {product.productImage && (
-        <img src={product.productImage} alt={product.productName} className='w-96 h-96 mb-4' />
+        <Image src={product.productImage} alt={product.productName} height={300} width={300} ></Image>
       )}
-      <p className='text-lg mb-2'><strong>Description:</strong> {product.description}</p>
-      <p className='text-lg mb-2'><strong>Category:</strong> {product.category}</p>
-      <p className='text-lg mb-2'><strong>Price:</strong> ${product.price}</p>
-      <Link href={`/Checkout/${productId}`} className='bg-colorOne text-white px-5 py-2 flex items-center'>Buy now</Link>
-      <button
-        onClick={handleAddToCart}
-        className='bg-colorOne text-white px-5 py-2 flex items-center mt-2'
-      >
-        Add to Cart
-      </button>
+      <p className='text-lg'><strong>Description:</strong> {product.description}</p>
+      <p className='text-lg'><strong>Category:</strong> {product.category}</p>
+      <p className='text-lg'><strong>Price:</strong> ${product.price}</p>
+      <PrimaryButton href={`/Checkout/${productId}`} value="Buy now"/>
+      <PrimaryButton onClickFunc={handleAddToCart} value="Add to Cart" disabled={false} />
+
       {/* <button
         onClick={handleRemoveFromCart}
-        className='bg-red-600 text-white px-5 py-2 flex items-center mt-2'
+        className='bg-red-600 text-white px-5 py-2 flex items-center text-center mt-2'
       >
         Remove from Cart
       </button> */}
